c596d296d403bd87576fd5f66e0267f1577ddbeb
Hao Ye <lhopitalified@gmail.com>
2018-02-06 11:21:51 -0500

    adding startup message warning about dev branch

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,8 @@ loadModule("block_lnlp_module", TRUE)
 loadModule("xmap_module", TRUE)
 
 .onAttach <- function(...) {
+    packageStartupMessage("*** WARNING *** This is the `dev` branch of the rEDM package. *** WARNING ***")
+    
     if (!interactive()) return()
     
     intro_message <- paste("If you're new to the rEDM package, please check out the tutorial:",
